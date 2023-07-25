568f1762fad2e64cfedc3b1dd3702c2f982e320f
Tomas <viltetomas2003@gmail.com>
2023-07-25 00:41:55 -0300

    test: Test: TestGetData

--- SteamAPI/main.go
+++ SteamAPI/main.go
@@ -9,11 +9,14 @@ func main() {
 	//fmt.Println(config.GetCrendentials())
 	//file := "/home/tomi/GCPSteamAnalytics/SteamAPI/api/data/hola2.txt"
 	//fmt.Println(utilities.UploadFileToGCS(file, "steam-analytics", "hola12.txt"))
-
-	res, _ := handlers.GetData()
+	dataFetcher := &handlers.RealDataFetcher{}
+	res, err := dataFetcher.GetData()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 	for _, v := range res {
 		fmt.Println(v)
 	}
-	fmt.Println(handlers.GetData())
 
 }
