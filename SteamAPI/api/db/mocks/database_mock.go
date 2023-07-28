31791298bd6295866da23c1830ff2528ffd55647
Tomas <viltetomas2003@gmail.com>
2023-07-27 23:15:25 -0300

    Testing function InsertBatchData

--- SteamAPI/api/db/mocks/database_mock.go
+++ SteamAPI/api/db/mocks/database_mock.go
@@ -1,10 +1,15 @@
 package mocks
 
-import "errors"
+import (
+	"errors"
+	"steamAPI/api/handlers"
+)
 
 type MockDatabase struct {
-	Connected  bool
-	ShouldFail bool // Nuevo campo para indicar si debe simular un error
+	Connected     bool
+	ShouldFail    bool            // Nuevo campo para indicar si debe simular un error
+	InsertedItems []handlers.Item // Nuevo campo para almacenar los items insertados en el mock
+	ShouldInsert  bool            // Nuevo campo para indicar si debe simular una inserción exitosa o fallida
 }
 
 func (m *MockDatabase) Connect() error {
@@ -21,4 +26,25 @@ func (m *MockDatabase) Close() error {
 	// Simular el cierre de la conexión
 	m.Connected = false
 	return nil
+}
+
+// InsertBatchData Implementar la función InsertBatchData para el mock
+func (m *MockDatabase) InsertBatchData(items []handlers.Item) error {
+	if m.ShouldFail {
+		return errors.New("error de inserción simulado")
+	}
+
+	// Simular una inserción exitosa o fallida según el valor de ShouldInsert
+	if m.ShouldInsert {
+		m.InsertedItems = append(m.InsertedItems, items...)
+	} else {
+		m.InsertedItems = []handlers.Item{} // Simular una inserción fallida eliminando los items insertados
+	}
+
+	return nil
+}
+
+// GetInsertedItems Función de ayuda para obtener los items insertados en el mock
+func (m *MockDatabase) GetInsertedItems() []handlers.Item {
+	return m.InsertedItems
 }
--- SteamAPI/main.go
+++ SteamAPI/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"steamAPI/api/db"
 	"steamAPI/api/handlers"
@@ -11,38 +10,11 @@ func main() {
 	//fmt.Println(config.GetCrendentials())
 	//file := "/home/tomi/GCPSteamAnalytics/SteamAPI/api/data/hola2.txt"
 	//fmt.Println(utilities.UploadFileToGCS(file, "steam-analytics", "hola12.txt"))
-	dataFetcher := &handlers.RealDataFetcher{}
-	data, err := dataFetcher.GetData()
+	fetcher := &handlers.RealDataFetcher{}
+	database := &db.MySQLDatabase{}
+
+	err := db.InsertData(fetcher, database)
 	if err != nil {
-		log.Println("Error al obtener datos:", err)
-		return
-	}
-	for _, v := range data {
-		fmt.Println(v)
-	}
-
-	dba := &db.MySQLDatabase{}
-
-	err = dba.Connect()
-	if err != nil {
-		log.Println("Error al conectar a Mysql:", err)
-	}
-	defer func() {
-		if err := dba.Close(); err != nil {
-			log.Println("Error al cerrar la conexión:", err)
-		}
-	}()
-
-	for _, item := range data {
-		newItem := handlers.Item{
-			Appid: item.Appid,
-			Name:  item.Name,
-		}
-		log.Println("Juego insertado en la tabla", newItem.Name)
-		err := dba.Insert(newItem)
-		if err != nil {
-			log.Printf("Error al insertar el elemento: %v", err)
-		}
-
+		log.Fatalf("Error al cargar los datos en la base de datos: %v", err)
 	}
 }
