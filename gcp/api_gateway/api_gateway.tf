7ee66d682285d08dc8fcbd6e06dacd734e1792f3
Tomas <viltetomas2003@gmail.com>
2023-07-28 18:09:16 -0300

    Configure endpoint

--- gcp/api_gateway/api_gateway.tf
+++ gcp/api_gateway/api_gateway.tf
@@ -1,6 +1,20 @@
 # Creamos la API en Api gateway
-resource "google_api_gateway_api" "api" {
-  api_id = "api"
-  project = "gcpsteamanalytics"
-  location = "us-central1"
+resource "google_api_gateway_api" "my_api" {
+    api_id = "my-api"
+    project = "gcpsteamanalytics"
+    location = "us-central1"
+}
+
+# Configuramos el endpoint de la API
+resource "google_api_gateway_config" "my_api_config" {
+    appid = google_api_gateway_api.my_api.id
+    project = "gcpsteamanalytics"
+    location = "us-central1"
+    backend {
+        google_cloud_function {
+            function = "ProcessSteamDataAndSaveToStorage"
+            path = "steamAPI/api/functions"
+            
+        }
+    }
 }
