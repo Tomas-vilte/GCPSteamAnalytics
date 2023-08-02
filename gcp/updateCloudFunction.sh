8a5495ddd82653dcbef4598c4dd99ca6cb281d1e
Tomas <viltetomas2003@gmail.com>
2023-08-02 20:27:22 -0300

    change entry point my cloud function

--- gcp/updateCloudFunction.sh
+++ gcp/updateCloudFunction.sh
@@ -4,4 +4,4 @@ zip -r steam.zip go.mod main.go go.sum utilities handlers functionGCP
 
 gsutil cp steam.zip gs://steam-analytics
 
-gcloud functions deploy process-steam-analytics --region=us-central1 --source=gs://steam-analytics/steam.zip
+gcloud functions deploy process-steam-analytics --region=us-central1 --source=gs://steam-analytics/steam.zip --entry-point=ProcessSteamDataAndSaveToStorage
