6cff1cc3640955cbaf688970dda39169c92d2c01
Tomas <viltetomas2003@gmail.com>
2023-08-02 15:14:26 -0300

    add file for deploy of cloud function

--- gcp/deploy.sh
+++ gcp/deploy.sh
@@ -5,10 +5,13 @@ zip -r steam.zip go.mod main.go go.sum utilities handlers functionGCP
 gsutil cp steam.zip gs://steam-analytics
 
 cd gcp
+
+terraform fmt
+
 terraform init
 
 terraform plan
 
 terraform apply --auto-approve
 
-#gcloud functions deploy process-steam-analytics --region=us-central1 --source=gs://steam-analytics/steam.zip
+gcloud functions deploy process-steam-analytics --region=us-central1 --source=gs://steam-analytics/steam.zip
