1eb116fbd9d05cad51c07b7887f8fd7ded2cbd09
Tomas <viltetomas2003@gmail.com>
2023-09-08 17:15:13 -0300

    refactor code

--- controller/game_controller.go
+++ controller/game_controller.go
@@ -1,9 +1,7 @@
 package controller
 
 import (
-	"fmt"
 	steamapi "github.com/Tomas-vilte/GCPSteamAnalytics/steamapi/model"
-	"github.com/Tomas-vilte/GCPSteamAnalytics/steamapi/persistence/entity"
 	"github.com/gin-gonic/gin"
 	"math"
 	"net/http"
@@ -11,68 +9,46 @@ import (
 )
 
 func (gc *GameControllers) GetGames(c *gin.Context) {
-	games, err := gc.dbClient.GetAllGames()
-	if err != nil {
-		c.JSON(400, gin.H{
-			"Error al obtener los juegos:": err.Error(),
-		})
-		return
-	}
-	gc.PaginateGames(c, games, 5)
-}
-
-func (gc *GameControllers) PaginateGames(c *gin.Context, games []entity.GameDetails, pageSize int) {
 	page := c.DefaultQuery("page", "1")
-	size := c.DefaultQuery("size", strconv.Itoa(pageSize))
+	pageSize := c.DefaultQuery("page_size", "10")
 
-	pageInt, _ := strconv.Atoi(page)
-	sizeInt, _ := strconv.Atoi(size)
-	cacheKey := generateCacheKey(page, size)
-	cachedResponse, err := gc.redisClient.GetGames(cacheKey)
+	pageNum := parseParam(page)
+	pageSizeNum := parseParam(pageSize)
 
-	if err == nil {
-		c.JSON(200, cachedResponse)
+	startIndex := (pageNum - 1) * pageSizeNum
+
+	games, totalItems, err := gc.dbClient.GetGamesByPage(startIndex, pageSizeNum)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	paginatedGames := getPaginatedGames(games, pageInt, sizeInt)
-
-	metadata := generateMetadata(len(games), pageInt, sizeInt)
+	metadata := generateMetadata(totalItems, pageNum, pageSizeNum)
 	response := steamapi.PaginatedResponse{
 		Metadata: metadata,
-		Games:    paginatedGames,
-	}
-
-	err = gc.SaveToCache(cacheKey, response)
-	if err != nil {
-		c.JSON(404, gin.H{
-			"Error al cachear los juegos:": err,
-		})
+		Games:    games,
 	}
 
 	c.JSON(http.StatusOK, response)
 }
 
-func generateCacheKey(page, size string) string {
-	return fmt.Sprintf("games_page_%s_size_%s", page, size)
-}
-
-func getPaginatedGames(games []entity.GameDetails, page, size int) []entity.GameDetails {
-	start := (page - 1) * size
-	end := start + size
-
-	if end > len(games) {
-		end = len(games)
+func parseParam(param string) int {
+	num, err := strconv.Atoi(param)
+	if err != nil || num <= 0 {
+		return 1
 	}
-
-	return games[start:end]
+	return num
 }
 
 func generateMetadata(total, page, size int) map[string]interface{} {
-	return map[string]interface{}{
+	totalPages := int(math.Ceil(float64(total) / float64(size)))
+
+	metadata := map[string]interface{}{
 		"total":       total,
 		"page":        page,
 		"size":        size,
-		"total_pages": int(math.Ceil(float64(total) / float64(size))),
+		"total_pages": totalPages,
 	}
+
+	return metadata
 }
