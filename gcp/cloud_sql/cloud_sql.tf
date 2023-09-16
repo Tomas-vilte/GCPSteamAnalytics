aa82644f13dff82d981c7e43020704b2298bdbae
Tomas <viltetomas2003@gmail.com>
2023-09-15 23:10:08 -0300

    config tf

--- gcp/cloud_sql/cloud_sql.tf
+++ gcp/cloud_sql/cloud_sql.tf
@@ -1,6 +1,6 @@
 resource "google_compute_instance" "my_instance" {
   name         = "my-instance"
-  machine_type = "e2-micro"
+  machine_type = "db-f1-micro"
   zone         = "us-central1-a"
 
   boot_disk {
@@ -8,29 +8,42 @@ resource "google_compute_instance" "my_instance" {
       image = "ubuntu-os-cloud/ubuntu-2004-lts"
     }
   }
+  
 
   network_interface {
     network = "default"
 
   }
+  
 }
 
 resource "google_sql_database_instance" "my_db_instance" {
   name             = "my-db-instance"
-  database_version = "MYSQL_5_7"
+  database_version = "MYSQL_8_0"
   region           = "us-central1"
   settings {
     tier = "db-f1-micro"
   }
+
+  # Configuración de la lista de redes autorizadas
+  settings {
+    ip_configuration {
+      ipv4_enabled    = true
+      authorized_networks {
+        name = "my-authorized-network"
+        value = "181.165.142.76"
+      }
+    }
+  }
 }
 
 resource "google_sql_user" "my_db_user" {
-  name     = "my-db-user"
+  name     = "root"
   instance = google_sql_database_instance.my_db_instance.name
   password = "root"
 }
 
 resource "google_sql_database" "my_db" {
-  name     = "my-database"
+  name     = "steamAnalytics"
   instance = google_sql_database_instance.my_db_instance.name
 }
