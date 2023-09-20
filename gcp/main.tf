92abf691518ae7b90702ffe0f34b2168e14ab105
Tomas <viltetomas2003@gmail.com>
2023-09-20 16:01:47 -0300

    deploy cloud sql

--- gcp/main.tf
+++ gcp/main.tf
@@ -7,17 +7,17 @@ provider "google" {
 #   source = "./cloud_storage"
 # }
 
-module "cloud_function" {
-  source = "./cloud_function"
-}
+# module "cloud_function" {
+#   source = "./cloud_function"
+# }
 
 # module "api_gateway" {
 #   source = "./api_gateway"
 # }
   
-# module "cloud_sql" {
-#   source = "./cloud_sql"
-# }
+module "cloud_sql" {
+  source = "./cloud_sql"
+}
 
 # module "redis" {
 #   source = "./memory_storage_redis"
