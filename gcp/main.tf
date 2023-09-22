254beacc9271a74cbe5946ae52f58c8dd93524e7
Tomas <viltetomas2003@gmail.com>
2023-09-22 14:54:17 -0300

    add documentacion API

--- gcp/main.tf
+++ gcp/main.tf
@@ -7,28 +7,29 @@ provider "google" {
 #   source = "./cloud_storage"
 # }
 
-module "cloud_function" {
-  DB_USER = var.DB_USER
-  DB_PASS = var.DB_PASS
-  DB_NAME = var.DB_NAME
-  INSTANCE_CONNECTION_NAME = var.INSTANCE_CONNECTION_NAME
-  REDISHOST = var.REDISHOST
-  source = "./cloud_function" 
-}
+# module "my_network" {
+#   source = "./vpc"
+# }
 
 # module "api_gateway" {
 #   source = "./api_gateway"
 # }
   
+
+# module "cloud_function" {
+#   DB_USER = var.DB_USER
+#   DB_PASS = var.DB_PASS
+#   DB_NAME = var.DB_NAME
+#   INSTANCE_CONNECTION_NAME = var.INSTANCE_CONNECTION_NAME
+#   REDISHOST = var.REDISHOST
+#   source = "./cloud_function" 
+# }
+
 # module "cloud_sql" {
 #   source = "./cloud_sql"
 # }
 
-module "redis" { 
-  source = "./memory_storage_redis"
+# module "redis" { 
+#   source = "./memory_storage_redis"
 
-}
-
-# module "my_network" {
-#   source = "./vpc"
 # }
