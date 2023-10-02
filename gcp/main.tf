c90d62fbaf63b3c1a35bb7a10e91d25500955655
Tomas <viltetomas2003@gmail.com>
2023-10-02 17:52:20 -0300

    load data in BigQuery

--- gcp/main.tf
+++ gcp/main.tf
@@ -3,36 +3,36 @@ provider "google" {
   region  = "US"
 }
 
-module "cloud_storage" {
-  source = "./cloud_storage"
-}
+# module "cloud_storage" {
+#   source = "./cloud_storage"
+# }
 
-module "my_network" {
-  source = "./vpc"
-}
+# module "my_network" {
+#   source = "./vpc"
+# }
 
-module "api_gateway" {
-  source = "./api_gateway"
-}
+# module "api_gateway" {
+#   source = "./api_gateway"
+# }
   
-module "cloud_function" {
-  DB_USER = var.DB_USER
-  DB_PASS = var.DB_PASS
-  DB_NAME = var.DB_NAME
-  INSTANCE_CONNECTION_NAME = var.INSTANCE_CONNECTION_NAME
-  REDISHOST = var.REDISHOST
-  source = "./cloud_function" 
-}
+# module "cloud_function" {
+#   DB_USER = var.DB_USER
+#   DB_PASS = var.DB_PASS
+#   DB_NAME = var.DB_NAME
+#   INSTANCE_CONNECTION_NAME = var.INSTANCE_CONNECTION_NAME
+#   REDISHOST = var.REDISHOST
+#   source = "./cloud_function" 
+# }
 
-module "cloud_sql" {
-  source = "./cloud_sql"
-}
+# module "cloud_sql" {
+#   source = "./cloud_sql"
+# }
 
-module "redis" { 
-  source = "./memory_storage_redis"
+# module "redis" { 
+#   source = "./memory_storage_redis"
 
-}
+# }
 
-module "cloud_composer" {
-  source = "./cloud_composer"
-}
+# module "cloud_composer" {
+#   source = "./cloud_composer"
+# }
