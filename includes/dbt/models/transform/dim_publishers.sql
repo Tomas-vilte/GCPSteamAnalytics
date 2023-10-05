7fedc00322bf9629550cab21a6ddd121eb5c68dc
Thomas <viltetomas2003@gmail.com>
2023-10-05 19:42:23 -0300

    create dim platform

--- includes/dbt/models/transform/dim_publishers.sql
+++ includes/dbt/models/transform/dim_publishers.sql
@@ -5,10 +5,11 @@ WITH publishers_cte AS (
     SELECT
         DISTINCT publishers AS publishers_name
     FROM {{ source('games', 'raw_games') }}
+    WHERE publishers IS NOT NULL
 )
 
 SELECT
-    ROW_NUMBER() OVER () AS publisher_id,
+    ROW_NUMBER() OVER() AS publisher_id,
     publishers_name
 FROM
     publishers_cte
--- includes/dbt/target/run/games/models/transform/dim_publishers.sql
+++ includes/dbt/target/run/games/models/transform/dim_publishers.sql
@@ -16,10 +16,11 @@ WITH publishers_cte AS (
     SELECT
         DISTINCT publishers AS publishers_name
     FROM `pristine-flames-400818`.`games`.`raw_games`
+    WHERE publishers IS NOT NULL
 )
 
 SELECT
-    ROW_NUMBER() OVER () AS publisher_id,
+    ROW_NUMBER() OVER() AS publisher_id,
     publishers_name
 FROM
     publishers_cte
