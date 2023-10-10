64dc1c47386ffb037cd157cc7fb7931103abf600
Thomas <viltetomas2003@gmail.com>
2023-10-10 20:52:45 -0300

    change query

--- includes/dbt/models/transform/dim_price_overview.sql
+++ includes/dbt/models/transform/dim_price_overview.sql
@@ -3,23 +3,46 @@
 -- Calculamos el precio final con impuesto considerando el descuento
 WITH price_overview_cte AS (
     SELECT
-        *,
-        CASE
-            WHEN discount_percent > 0 THEN initial_price * (1 - discount_percent / 100)
-            ELSE 0.0  
-        END AS discounted_price,
-        CONCAT('ARS$ ',
-            CASE
-                WHEN discount_percent > 0 THEN CAST(ROUND(initial_price * (1 - discount_percent / 100), 2) AS STRING)
-                ELSE 'ARS$ 0.00'  
-            END
-        ) AS formatted_discounted_price,
-        CONCAT('ARS$ ', CAST(ROUND((initial_price * (1 - discount_percent / 100)) * 1.75, 2) AS STRING)) AS formatted_price_with_tax
+        initial_price,
+        final_price,
+        discount_percent AS discount_pct,
+        formatted_initial_price,
+        formatted_final_price,
+        -- Monto del Descuento formateado como decimal con dos decimales
+       CAST(
+        (CASE
+            WHEN discount_percent > 0 THEN initial_price * (discount_percent / 100)
+            ELSE 0.0
+        END) / 100 AS NUMERIC
+    ) AS discount_amount,
+
+    -- Precio del Juego con Impuesto (75%)
+    (CASE
+    WHEN discount_percent > 0 THEN
+        CAST((initial_price * (1 - discount_percent / 100) * 1.75) AS NUMERIC)
+    ELSE
+        CAST(initial_price * 1.75 AS NUMERIC)
+    END) AS price_with_tax
     FROM
     {{ source('games', 'raw_games') }}
 )
 
 SELECT
     ROW_NUMBER() OVER() AS purchase_id,
-    *
+    initial_price,
+    final_price,
+    discount_pct,
+    discount_amount,
+    formatted_initial_price,
+    formatted_final_price,
+    price_with_tax,
+    CONCAT('ARS$ ', REPLACE(FORMAT('%.2f', price_with_tax), '.', ',')) AS formatted_price_with_tax
 FROM price_overview_cte
+WHERE 
+    (initial_price IS NOT NULL)
+    AND (final_price IS NOT NULL)
+    AND (discount_pct IS NOT NULL)
+    AND (discount_amount IS NOT NULL)
+    AND (formatted_initial_price IS NOT NULL)
+    AND (formatted_final_price IS NOT NULL)
+    AND (price_with_tax IS NOT NULL)
