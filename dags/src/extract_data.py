4f3977f6639c114f2eea86be6dfe93a28034a232
Thomas <viltetomas2003@gmail.com>
2023-10-17 14:07:43 -0300

    add xcoms

--- dags/src/extract_data.py
+++ dags/src/extract_data.py
@@ -9,9 +9,8 @@ dir: Path = Path(__file__).resolve().parent.parent.parent
 dataPath: Path = Path(f"{dir}/includes/dataset/")
 
 
-def extract_data_games_details() -> Tuple[List, List]:
-    data: list = []
-    column_names: list = []
+def extract_data_games_details(**kwargs):
+    data, column_names = [], []
     try:
         with DatabaseConnection().get_connection() as connection:
             if connection:
@@ -19,24 +18,27 @@ def extract_data_games_details() -> Tuple[List, List]:
                     cursor.execute("SELECT * FROM games_details")
                     column_names = [column[0] for column in cursor.description]
                     rows = cursor.fetchall()
-
                     for row in rows:
                         data.append(row)
-
     except Exception as e:
         logs.info(f"Error al extraer datos: {str(e)}")
-    return data, column_names
+    kwargs['ti'].xcom_push(key='data', value=data)
+    kwargs['ti'].xcom_push(key='column_names', value=column_names)
 
 
-def save_data_to_csv(data: list, column_names: list) -> None:
+def save_data_to_csv(**kwargs):
+    ti = kwargs['ti']
+    data = ti.xcom_pull(task_ids='extract_data', key='data')
+    column_names = ti.xcom_pull(task_ids='extract_data', key='column_names')
+
     try:
         column_names_lower = [name.lower() for name in column_names]
         with open(
-            f"{dataPath}/games_details_{datetime.date.today()}.csv", "w", newline=""
+                f"{dataPath}/games_details_{datetime.date.today()}.csv", "w", newline=""
         ) as csv_file:
             csv_writer = csv.writer(csv_file)
             csv_writer.writerow(column_names_lower)
             csv_writer.writerows(data)
+        logs.info(f"Se cargaron los datos en el archivo CSV: {csv_file.name}")
     except Exception as e:
         logs.error(f"Error al guardar los datos en CSV:{str(e)}")
-    logs.info(f"Se cargaron los datos en el archivo CSV: {csv_file.name}")
